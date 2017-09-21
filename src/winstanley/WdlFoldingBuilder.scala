056755873c5056753c5f62d080edaba251437137
Chris Llanwarne <cjllanwarne@gmail.com>
2017-09-21 18:36:31 -0400

    Collapsible blocks in the WDL editor

---
+++ src/winstanley/WdlFoldingBuilder.scala
@@ -0,0 +1,20 @@
+package winstanley
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.{FoldingBuilderEx, FoldingDescriptor}
+import com.intellij.openapi.editor.Document
+import com.intellij.psi.PsiElement
+import winstanley.structure.WdlDocumentUtils._
+
+class WdlFoldingBuilder extends FoldingBuilderEx {
+
+  private def foldingRegions(psiElement: PsiElement): Array[FoldingDescriptor] = {
+    (psiElement.getChildren flatMap foldingRegions) ++ (psiElement.contentRange map { new FoldingDescriptor(psiElement.getNode, _)})
+  }
+
+  override def buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array[FoldingDescriptor] = foldingRegions(root)
+
+  override def getPlaceholderText(astNode: ASTNode): String = " ... "
+
+  override def isCollapsedByDefault(astNode: ASTNode): Boolean = false
+}
