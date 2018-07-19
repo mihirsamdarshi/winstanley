910e96655d531565e0ce01ba3558d43327e00404
Adam Nichols <anichols@broadinstitute.org>
2018-07-19 17:05:33 -0400

    Womstanley class & action

---
+++ src/winstanley/Womstanley.scala
@@ -0,0 +1,10 @@
+package winstanley
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class Womstanley extends AnAction {
+  override def actionPerformed(e: AnActionEvent): Unit = {
+    // TODO: insert action logic here
+  }
+}
