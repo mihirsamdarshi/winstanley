fb9058dc5394cc5a3c82f756af32500ec495b527
Adam Nichols <anichols@broadinstitute.org>
2018-07-19 19:41:01 -0400

    Inner class

--- src/winstanley/Womstanley.scala
+++ src/winstanley/Womstanley.scala
@@ -10,28 +10,37 @@ import womtool.WomtoolMain.{SuccessfulTermination, Termination, UnsuccessfulTerm
 import com.intellij.notification.NotificationType.{INFORMATION, ERROR}
 
 
-class Womstanley extends AnAction {
-  override def actionPerformed(e: AnActionEvent): Unit = {
+object Womstanley {
 
-    // TODO: proper error handling
-    // TODO: If focus is on something other than a file (like the console) this will fail
-    val currentFile: VirtualFile = e.getData(VIRTUAL_FILE).asInstanceOf[VirtualFile]
-    val path: Path = DefaultPathBuilder.build(currentFile.getPath).toOption.get
+  class Validate extends AnAction {
+    override def actionPerformed(e: AnActionEvent): Unit = {
 
-    // Maybe: find same-named inputs JSON and pass it in as second param?
-    val result: Termination = Validate.validate(path, None)
+      // TODO: proper error handling
+      // TODO: If focus is on something other than a file (like the console) this will fail
+      val currentFile: VirtualFile = e.getData(VIRTUAL_FILE).asInstanceOf[VirtualFile]
+      val path: Path = DefaultPathBuilder.build(currentFile.getPath).toOption.get
 
-    result match {
-      case success: SuccessfulTermination =>
-        // HTML works here!
-        val notification = new Notification("WOMtool", "WDL Validation Succeeded", "<i>" + currentFile.getName + "</i>", INFORMATION)
-        Notifications.Bus.notify(notification)
-        println(success.stdout)
-      case failure: UnsuccessfulTermination =>
-        val notification = new Notification("WOMtool", "WDL Validation Failed", failure.stderr.get, ERROR)
-        Notifications.Bus.notify(notification)
-        println(failure.stderr)
-      case _ => ??? // TODO: proper error handling
+      // Maybe: find same-named inputs JSON and pass it in as second param?
+      val result: Termination = Validate.validate(path, None)
+
+      result match {
+        case success: SuccessfulTermination =>
+          // HTML works here!
+          val notification = new Notification("WOMtool", "WDL Validation Succeeded", "<i>" + currentFile.getName + "</i>", INFORMATION)
+          Notifications.Bus.notify(notification, e.getProject) // I'm not exactly sure what specifying the (optional) project gets us, but it's free so why not
+          println(success.stdout)
+        case failure: UnsuccessfulTermination =>
+          val notification = new Notification("WOMtool", "WDL Validation Failed", failure.stderr.get, ERROR)
+          Notifications.Bus.notify(notification, e.getProject)
+          println(failure.stderr)
+        case _ => ??? // TODO: proper error handling
+      }
     }
   }
+
+  // Other WOMtool commands...
+//  class Inputs extends AnAction {
+//
+//  }
 }
+
