3977e1d590361bb2a7e7bb1801be593b67cd4be0
Adam Nichols <anichols@broadinstitute.org>
2018-08-01 18:08:12 -0400

    Considerably better

--- src/winstanley/Womstanley.scala
+++ src/winstanley/Womstanley.scala
@@ -1,5 +1,8 @@
 package winstanley
 
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.filters.TextConsoleBuilderFactory
 import com.intellij.notification.{Notification, Notifications}
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
 import com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE
@@ -7,7 +10,10 @@ import com.intellij.openapi.vfs.VirtualFile
 import cromwell.core.path.{DefaultPathBuilder, Path}
 import womtool.validate.Validate
 import womtool.WomtoolMain.{SuccessfulTermination, Termination, UnsuccessfulTermination}
-import com.intellij.notification.NotificationType.{INFORMATION, ERROR}
+import com.intellij.notification.NotificationType.{ERROR, INFORMATION}
+import com.intellij.execution.ui.ConsoleViewContentType.ERROR_OUTPUT
+import com.intellij.openapi.wm.ToolWindowId
+import javax.swing.Icon
 
 
 object Womstanley {
@@ -15,25 +21,41 @@ object Womstanley {
   class Validate extends AnAction {
     override def actionPerformed(e: AnActionEvent): Unit = {
 
-      // TODO: proper error handling
-      // TODO: If focus is on something other than a file (like the console) this will fail
-      val currentFile: VirtualFile = e.getData(VIRTUAL_FILE).asInstanceOf[VirtualFile]
-      val path: Path = DefaultPathBuilder.build(currentFile.getPath).toOption.get
+      // If focus is on something other than a file (like the console) we'll get null
+      val currentFile: VirtualFile = e.getData(VIRTUAL_FILE)
+      if (currentFile != null) {
+        val path: Path = DefaultPathBuilder.build(currentFile.getPath).toOption.get
 
-      // Maybe: find same-named inputs JSON and pass it in as second param?
-      val result: Termination = Validate.validate(path, None)
+        // Maybe: find same-named inputs JSON and pass it in as second param?
+        val result: Termination = Validate.validate(path, None)
 
-      result match {
-        case success: SuccessfulTermination =>
-          // HTML works here!
-          val notification = new Notification("WOMtool", "WDL Validation Succeeded", "<i>" + currentFile.getName + "</i>", INFORMATION)
-          Notifications.Bus.notify(notification, e.getProject) // I'm not exactly sure what specifying the (optional) project gets us, but it's free so why not
-          println(success.stdout)
-        case failure: UnsuccessfulTermination =>
-          val notification = new Notification("WOMtool", "WDL Validation Failed", failure.stderr.get, ERROR)
-          Notifications.Bus.notify(notification, e.getProject)
-          println(failure.stderr)
-        case _ => ??? // TODO: proper error handling
+        result match {
+          case success: SuccessfulTermination =>
+            // HTML works here!
+            val notification = new Notification("WomTool", "WDL Validation Succeeded", "<i>" + currentFile.getName + "</i>", INFORMATION)
+            Notifications.Bus.notify(notification, e.getProject) // I'm not exactly sure what specifying the (optional) project gets us, but it's free so why not
+//            println(success.stdout)
+          case failure: UnsuccessfulTermination =>
+
+            import com.intellij.openapi.wm.ToolWindow
+            import com.intellij.openapi.wm.ToolWindowManager
+
+            val toolWindow = ToolWindowManager.getInstance(e.getProject).getToolWindow("Event Log")
+
+            assert(toolWindow != null, "Attempted to write console output to non-existent tool window")
+
+            val console = TextConsoleBuilderFactory.getInstance().createBuilder(e.getProject).getConsole
+            val content = toolWindow.getContentManager.getFactory.createContent(console.getComponent, "WDL Messages", true)
+
+            toolWindow.getContentManager.addContent(content)
+            toolWindow.getContentManager.setSelectedContent(content, true, true)
+
+            toolWindow.activate(null)
+
+            console.print(failure.stderr.get, ERROR_OUTPUT)
+
+          case _ => ??? // TODO: proper error handling
+        }
       }
     }
   }
